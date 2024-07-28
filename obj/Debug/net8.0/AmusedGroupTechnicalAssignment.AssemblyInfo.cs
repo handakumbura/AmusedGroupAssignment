22791c3d9b1b1220f3d0a044cd387a8c0893e616
handakumbura <dumidu@gmail.com>
2024-07-28 12:24:07 +0530

    clean the project.

--- obj/Debug/net8.0/AmusedGroupTechnicalAssignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/AmusedGroupTechnicalAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmusedGroupTechnicalAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c7c8e9602edb46d6ca1d00c60f4eab5ea732308")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmusedGroupTechnicalAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmusedGroupTechnicalAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
